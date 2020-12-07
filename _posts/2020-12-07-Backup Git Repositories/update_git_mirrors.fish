8ec7be1a50e54997d58837658a410154fe5f362c
Jordan Williams <19399197+jwillikers@users.noreply.github.com>
2020-12-07 07:50:02 -0600

    Publish Backup Git Repositories
    Former-commit-id: a946ff32e04b568caddda6e207abf516670ed759

---
+++ _posts/2020-12-07-Backup Git Repositories/update_git_mirrors.fish
@@ -0,0 +1,15 @@
+function update_git_mirrors -d "For each directory given, non-recursively update each Git mirror repository directory suffixed with .git"
+  for dir in $argv
+    if not test -d $dir
+      continue
+      echo "Argument '$dir' is not a directory" 1>&2
+    end
+
+    for mirror in $dir/*.git
+      if test -d $mirror
+         git -C $mirror remote update --prune >/dev/null
+         echo "Updated $mirror"
+      end
+    end
+  end
+end
